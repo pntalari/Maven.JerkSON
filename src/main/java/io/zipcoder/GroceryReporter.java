ed8fedd64978e21caa59a198a4d7b6fed67210a7
Nirmala <nirmala.talari@gmail.com>
2019-03-18 16:39:32 -0400

    ReporterInProgress

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,17 +1,33 @@
 package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.Item;
+import io.zipcoder.utils.ItemParseException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroceryReporter {
     private final String originalFileText;
+    ItemParser parser = new ItemParser();
 
     public GroceryReporter(String jerksonFileName) {
-
         this.originalFileText = FileReader.readFile(jerksonFileName);
     }
 
     @Override
     public String toString() {
+        List<Item> list = new ArrayList<>();
+        try {
+            parser.parseItemList(originalFileText);
+
+
+
+        } catch (ItemParseException e) {
+            e.printStackTrace();
+        }
+
+
         return null;
     }
 }
